7f48219b34e95bb9753fb7c2c7982928ae9b47e6
Mike "Ozzie" Osmonson <mikeosmonson@gmail.com>
2025-01-10 11:57:07 -0700

    Complete dragon class

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
+class Dragon {
+    constructor(name, rider, hungry) {
+        this.name = name;
+        this.rider = rider;
+        this.hungry = true;
+        this.eat_count = 0;
+    }
 
+    greet() {
+        return `Hi, ${this.rider}!`
+    }
+
+    eat() {
+        this.eat_count++;
+        if(this.eat_count == 3) {
+            this.hungry = false;
+        }
+    }
+}
+
+module.exports = Dragon;
