0261c1e6f465d884d4e855202182c39b2c66ba53
Mike "Ozzie" Osmonson <mikeosmonson@gmail.com>
2025-01-15 11:52:32 -0700

    Finish Vampire class

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -2,7 +2,17 @@ class Vampire {
     constructor(name, pet = 'bat') {
         this.name = name;
         this.pet = pet;
+        this.thirsty = true;
+        this.ouncesDrank = 0;
     };
+
+    drink() {
+        if(this.ouncesDrank == 50) {
+            return 'I\'m too full to drink anymore!'
+        }
+        this.ouncesDrank += 10;
+        this.thirsty = false;
+    }
 }
 
 module.exports = Vampire;
