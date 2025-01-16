254b78327fa47e0bc4f6db21134d30387160db3d
Mike "Ozzie" Osmonson <mikeosmonson@gmail.com>
2025-01-16 15:05:46 -0700

    Add hungry state and how it changes

--- mythical-creatures/exercises/werewolf.js
+++ mythical-creatures/exercises/werewolf.js
@@ -3,14 +3,17 @@ class Werewolf {
     constructor(name) {
         this.name = name;
         this.form = 'human';
+        this.hungry = false;
     };
 
     completeTransformation() {
         if (this.form == 'human') {
             this.form = 'wolf';
+            this.hungry = true;
             return 'Aaa-Woooo!';
         } else {
             this.form = 'human';
+            this.hungry = false;
             return 'Where are I?';
         }
     };
