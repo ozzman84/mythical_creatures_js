105000ddf507b1c96240c6010afd204315b7ec9b
Mike "Ozzie" Osmonson <mikeosmonson@gmail.com>
2025-01-26 11:52:03 -0700

    Add Craft that can complete total costs

--- mythical-creatures/crafting/src/craft.js
+++ mythical-creatures/crafting/src/craft.js
@@ -1,5 +1,25 @@
-class Craft {
+var Material = require("../src/material.js");
 
+class Craft {
+    constructor(params) {
+        this.name = params.type;
+        this.materials = params.materials;
+        this.completed = false;
+    }
+
+    completeCraft() {
+        this.completed = true;
+        return 'All done! It looks great!'
+    }
+
+    calculateProjectTotal() {
+        let totalCost = 0
+    
+        for (let i = 0; i < this.materials.length; i++) {
+            totalCost += this.materials[i].cost * this.materials[i].quantity
+        }
+        return totalCost
+    }
 }
 
 module.exports = Craft;
