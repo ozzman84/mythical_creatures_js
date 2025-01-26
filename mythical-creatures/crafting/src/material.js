c90aca12df01eaaaa5bf6ed5328a3c19533c6963
Mike "Ozzie" Osmonson <mikeosmonson@gmail.com>
2025-01-26 11:52:59 -0700

    Add Materials class

--- mythical-creatures/crafting/src/material.js
+++ mythical-creatures/crafting/src/material.js
@@ -1,5 +1,10 @@
 class Material {
-
+    constructor(name, cost, quantity, type) {
+        this.name = name;
+        this.cost = cost;
+        this.quantity = quantity,
+        this.type = type;
+    }
 }
 
 module.exports = Material;
