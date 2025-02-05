f7ab3fe965a14f26c4603926a34e6144ea9d8179
Mike "Ozzie" Osmonson <mikeosmonson@gmail.com>
2025-02-04 21:22:49 -0700

    Add condition to repair

--- mythical-creatures/oregon-trail/src/part.js
+++ mythical-creatures/oregon-trail/src/part.js
@@ -2,6 +2,7 @@ class Part {
     constructor(type) {
         this.type = type;
         this.broken = false;
+        this.repairCount = 0;
     }
 
     break() {
@@ -9,7 +10,12 @@ class Part {
     }
 
     repair() {
+        if (this.repairCount >= 1) {
+            return `This ${this.type} has seen better days, we\'ll need a brand new one!`
+        }
+    
         this.broken = false;
+        this.repairCount++;
     }
 }
 
