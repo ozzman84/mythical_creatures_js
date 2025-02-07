7d31443991a14c7612e3471a3d569593d063f3bd
Mike "Ozzie" Osmonson <mikeosmonson@gmail.com>
2025-02-07 12:39:56 -0700

    Add functions to add distress and update status

--- mythical-creatures/oregon-trail/src/settler.js
+++ mythical-creatures/oregon-trail/src/settler.js
@@ -6,6 +6,25 @@ class Settler {
         this.status = 'healthy';
         this.ailments = [];
     }
+
+    experienceDistress(distress) {
+        if (this.status === 'dead') return;
+    
+        this.ailments.push(distress);
+        this.updateStatus();
+    }
+
+    updateStatus() {
+        let ailmentsCount = this.ailments.length
+
+        if (ailmentsCount > 2) {
+            this.status = 'dead';
+        } else if (ailmentsCount === 2) {
+            this.status = 'poor';
+        } else if (ailmentsCount === 1) {
+            this.status = 'fair';
+        }
+    }
 }
 
 module.exports = Settler;
