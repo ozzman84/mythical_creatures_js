a819625e8002845944fe7c2c23df7f0117b90346
Mike "Ozzie" Osmonson <mikeosmonson@gmail.com>
2025-02-13 09:05:24 -0700

    Add function to attempt to learn new tricks

--- mythical-creatures/skate-park/skater.js
+++ mythical-creatures/skate-park/skater.js
@@ -6,6 +6,16 @@ class Skater {
         this.money = params.cash;
         this.frustration = 0;
     }
+
+    practice(trick) {
+        if (!this.tricks[trick]) {
+            this.frustration++;
+            if (this.frustration === 3) {
+                this.frustration = 0;
+                return this.learnNewTrick(trick);
+            }
+        }
+    }
 }
 
 module.exports = Skater;
